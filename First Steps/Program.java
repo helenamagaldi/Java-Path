01dd1c0171064ba41ab6b889b7271c306d296013
Helena Magaldi <helenamagaldi@gmail.com>
2020-09-30 10:01:53 -0300

    Updating README and adding First Program - Hello World

---
+++ First Steps/Program.java
@@ -0,0 +1,20 @@
+class Program { 
+
+// # general program
+// main function is the application entry.
+    public static void main(String[] args) { 
+        // reserved words in java are always written using lower case letters
+        // reserved here: publlic, static, void 
+// # indent, indent, indent because system out is inside main that is inside the program class 
+// always use "" for strings in .java 
+        System.out.println("hello world");
+        // every instruction for .java files needs to be finalized with ; 
+    } 
+}
+// { } keys set the program block, in this class public static void main for Program
+// enters and space are just stetic for .java - indenting is the important thing. using lines and spaces is interesting for a better understanding of the code 
+// # every java code needs to be inside a class
+// type javac fylename.java in order to check errors in your code 
+// to run the code, just write the class name generated in the program after "java"
+// for this exemple: java Program 
+// it shows hello world
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Java-Path
 Codes generated on Java Path - Alura
+
+
+JVM  understands certain commands. These commands are also called opcodes (operation code) e each opcode is exactly 1 Byte. So, we have a 1 Byte Opcode, or Bytecode. 
+So, it's the JVM that has the responsibility to execute bytecode.
+But both JVM and JRE execute bytecode.
+JRE contains JVM, but also has a lot of embebbed libraries - if we want the java application to work, you need libraries on top of the virtual machine. 
+
+JDK = JRE + development tools
